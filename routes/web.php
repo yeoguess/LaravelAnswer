bad1371d59b28782cd9747162966316e78d5beed
yeoguess <yogesh.maharjan3@gmail.com>
2019-08-05 20:44:41 +0545

    Files added

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+
+Route::get('/about', 'PageController@about')->name('about');
+Route::get('/contact', 'PageController@contact')->name('contact');
+Route::post('/contact', 'PageController@submit');
+Route::resource('questions', 'QuestionController');
